43758e2a9b941a7c0ebb782da0c6dc268d91e5a2
Tom OMalley <9475035+tomalley104@users.noreply.github.com>
2024-11-18 00:51:17 -0500

    add init and static funcs for testing

--- Sources/SDKTestPackage/SDKTestPackage.swift
+++ Sources/SDKTestPackage/SDKTestPackage.swift
@@ -4,6 +4,12 @@
 import sdk_framework
 
 public final class SDKTestPackage {
+    public static let shared: SDKTestPackage = .init()
+    public static func useFromSDKFrameworkSingleton() {
+        shared.useFromSDKFramework()
+    }
+
+    public init() { }
     public func useFromSDKFramework() {
         TestSDK.printVersion()
         TestSDK.shared.printVersion()
