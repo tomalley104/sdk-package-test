89c4e4ffd1c4e86ca0a24d780ad3e9d57bf88b30
Tom OMalley <9475035+tomalley104@users.noreply.github.com>
2024-11-19 17:00:58 -0500

    use swift tools 5.8, ios/catalyst v16

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SDKTestPackage",
-    platforms: [.iOS(.v17), .macCatalyst(.v17),],
+    platforms: [.iOS(.v16), .macCatalyst(.v16),],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
